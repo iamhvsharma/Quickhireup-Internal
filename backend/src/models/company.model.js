6fd372023a85ad3676811d6d64158db86c18ab11
iamhvsharma <itshvsharma@gmail.com>
2024-11-25 23:31:27 +0530

    Bug Fix

--- backend/src/models/company.model.js
+++ backend/src/models/company.model.js
@@ -35,7 +35,7 @@ const companySchema = new Schema({
       required: true,
     }],
     numberOfEmployees: {
-      type: Number,
+      type: String,
       enum: ['1-10', '11-50', '51-200', '201-500', '500+'],
       required: true,
     },
