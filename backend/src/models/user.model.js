261cf5e6d7f38c2e08d443d054a477d74432af34
Harshvardhan Sharma <itshvsharma@gmail.com>
2024-12-02 00:38:32 +0530

    Referesh token added

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
   name: {
@@ -41,6 +42,16 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isValidPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
+
+
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({
+    _id: this._id,
+  }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.REFERESH_TOKEN_EXPIRY, 
+  });
+}
 
 const User = mongoose.model('User', userSchema);
 
