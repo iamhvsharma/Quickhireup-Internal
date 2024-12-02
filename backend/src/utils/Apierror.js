5e4ff41cc2f294eeb3ffdbfe6bb6f58e332856c9
Harshvardhan Sharma <itshvsharma@gmail.com>
2024-12-03 03:15:16 +0530

    Enhancement

--- backend/src/utils/Apierror.js
+++ backend/src/utils/Apierror.js
@@ -1,4 +1,4 @@
-class Apierror extends Error {
+class ApiError extends Error {
   constructor(
     statusCode,
     message = "Something went wrong",
@@ -22,4 +22,4 @@ class Apierror extends Error {
 
 
 
-export { Apierror }
+export { ApiError }
